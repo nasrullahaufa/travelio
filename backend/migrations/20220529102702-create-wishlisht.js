b57bf976d39cfd468fb15536f66a9c6cb71bc910
nasrullahaufa <nasrullahaufa@gmail.com>
2022-05-29 17:37:16 +0700

    db sequelized

---
+++ backend/migrations/20220529102702-create-wishlisht.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Wishlishts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.NUMBER
+      },
+      wihslist: {
+        type: Sequelize.ARRAY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Wishlishts');
+  }
+};
